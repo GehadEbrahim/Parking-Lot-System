b7552c8b2a4cc231345c07ae73e892b01d0c73fc
GehadEbrahim <gebrahim091@gmail.com>
2024-12-04 06:15:31 +0200

    Admin.java

--- src/Admin.java
+++ src/Admin.java
@@ -1,5 +1,13 @@
 //Start Admin class
-public class Admin{
-  
-//Add attributes
+public class Admin extends User{
+
+
+    public Admin(String name, String password) {
+        super(name, password);
+    }
+
+    @Override
+    public int logIn(String name, String password) {
+        return 0;
+    }
 }
